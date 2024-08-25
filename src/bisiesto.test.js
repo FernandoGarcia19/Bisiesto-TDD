a1f473e33c786882de668c93e3a1e8707b95af5e
FernandoGarcia19 <fernando.garcia@ucb.edu.bo>
2024-08-25 02:38:54 -0400

    test 5 created

--- src/bisiesto.test.js
+++ src/bisiesto.test.js
@@ -13,4 +13,7 @@ describe("Calcular", () => {
   it("4: Deberia marcar 2016 como bisiesto", () => {
     expect(es_bisiesto(2016)).toEqual(true);
   });
+  it("5: Deberia marcar 2019 como NO bisiesto", () => {
+    expect(es_bisiesto(2019)).toEqual(false);
+  });
 });
