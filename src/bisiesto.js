00446ae8ec6a0951068ee5682b1d7c78c93c74bb
FernandoGarcia19 <fernando.garcia@ucb.edu.bo>
2024-08-25 02:37:34 -0400

    test 4 passed

--- src/bisiesto.js
+++ src/bisiesto.js
@@ -1,8 +1,11 @@
 function es_bisiesto(year){
     if(year % 400 == 0 && year % 100 == 0){
         return true;
-    }else if(year ^{
-        return false;
+    }else if(year % 4 === 0 && year % 100 != 0){
+        return true;
+    }
+    else{
+        return false
     }
 }
 
