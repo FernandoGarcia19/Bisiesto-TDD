3e61529a60acc05cf53965c50acf66dac65c900f
FernandoGarcia19 <fernando.garcia@ucb.edu.bo>
2024-08-25 02:28:51 -0400

    test 2 created

--- src/bisiesto.test.js
+++ src/bisiesto.test.js
@@ -4,4 +4,8 @@ describe("Calcular", () => {
   it("1: Deberia marcar 2000 como bisiesto", () => {
     expect(es_bisiesto(2000)).toEqual(true);
   });
+  it("2: Deberia marcar cualquier año dividido por 400 como bisiesto", () => {
+    expect(es_bisiesto(1600)).toEqual(true);
+  });
+
 });
